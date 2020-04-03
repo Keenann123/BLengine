4aad45cf5e533d091489045cd1264c2d009800c2
Samuel Hall <asagpol@gmail.com>
2020-04-04 00:41:50 +0100

    Add basic entity class not finished yet tho and change a few project settings :)

---
+++ BLengine/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace RenderingEngine
+{
+    class Entity
+    {
+        Vector3 position;
+        Quaternion rotation;
+        Vector3 scale;
+
+        public Entity(Vector3 pos, Quaternion rot, Vector3 scl)
+        {
+            position = pos;
+            rotation = rot;
+            scale = scl;
+        }
+    }
+}
